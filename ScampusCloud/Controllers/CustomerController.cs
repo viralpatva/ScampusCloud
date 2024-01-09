5a474e75aa212280d70ac35efd8eb01004e32c52
Scampus11 <103895051+Scampus11@users.noreply.github.com>
2024-01-09 21:08:55 +0530

    Customer design added

---
+++ ScampusCloud/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScampusCloud.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Customer()
+        {
+            return View();
+        }
+        public ActionResult AddEditCustomer()
+        {
+            return View();
+        }
+    }
+}
