4491d16c93d57784e7681c02af811cface2ee6df
Scampus11 <103895051+Scampus11@users.noreply.github.com>
2024-01-11 22:15:47 +0530

    customer model add

---
+++ ScampusCloud/Models/CustomerModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScampusCloud.Models
+{
+	public class CustomerModel : ResponseMessage
+	{
+		public CustomerModel()
+		{
+		}
+
+		public int? Id { get; set; }
+
+		public Guid? CompanyId { get; set; }
+
+		[Required(ErrorMessage = "Customer Name is required")]
+		public string CustomerName { get; set; }
+
+		[Required(ErrorMessage = "Last Name is required")]
+		public string LastName { get; set; }
+
+		[Required(ErrorMessage = "Company Name is required")]
+
+		public string CompanyName { get; set; }
+
+		[Required(ErrorMessage = "Contact Phone is required")]
+		[RegularExpression(@"^[+]{1}[0-9]{2}[0-9]{10}$", ErrorMessage = "Invalid Contact Phone Format")]
+		public string ContactPhone { get; set; }
+
+		[Required(ErrorMessage = "Email Address is required")]
+		[RegularExpression(@"^[_a-z0-9-]+(\.[_a-z0-9-]+)*(\+[a-z0-9-]+)?@[a-z0-9-]+(\.[a-z0-9-]+)*$", ErrorMessage = "Invalid Email Address Format")]
+		public string EmailAddress { get; set; }
+
+		[Required(ErrorMessage = "Company Site is required")]
+
+		[RegularExpression(@"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$", ErrorMessage = "Invalid Company Site Format")]
+		public string CompanySite { get; set; }
+		public Guid? CreatedBy { get; set; }
+		public Guid? ModifiedBy { get; set; }
+		public string ActionType { get; set; }
+	}
+}
---
+++ ScampusCloud/Repository/Customer/CustomerRepository.cs
@@ -0,0 +1,66 @@
+﻿using ScampusCloud.DataBase;
+using ScampusCloud.Models;
+using ScampusCloud.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace ScampusCloud.Repository.Customer
+{
+	public class CustomerRepository
+	{
+		private GeneralMethods objgm = new GeneralMethods();
+		public bool AddEdit_Customer(CustomerModel _CustomerModel)
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = _CustomerModel.GetType().Name,
+					StoredProcedureName = @"SP_Customer_CURD",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+
+				objQueryBuilder.AddFieldValue("@Id", _CustomerModel.Id, DataTypes.Numeric, false);
+				objQueryBuilder.AddFieldValue("@CompanyId", _CustomerModel.CompanyId, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Name", _CustomerModel.CustomerName +" "+ _CustomerModel.LastName, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Address", _CustomerModel.CompanyName, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Website", _CustomerModel.CompanySite, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@EmailId", _CustomerModel.EmailAddress, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@PhoneNumber", _CustomerModel.ContactPhone, DataTypes.Text, false);
+				//objQueryBuilder.AddFieldValue("@CreatedBy", _CustomerModel.CreatedBy, DataTypes.Text, false);
+				//objQueryBuilder.AddFieldValue("@ModifiedBy", _CustomerModel.ModifiedBy, DataTypes.Text, false); 
+				objQueryBuilder.AddFieldValue("@ActionType", _CustomerModel.ActionType, DataTypes.Text, false);
+				
+				return objgm.ExecuteObjectUsingSp<bool>(objQueryBuilder, true);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+
+		public CustomerModel GetCustomerList()
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					StoredProcedureName = @"SP_Customer_List",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+
+				return objgm.ExecuteObjectUsingSp<CustomerModel>(objQueryBuilder, true);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+	}
+}
