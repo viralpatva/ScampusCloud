703c28aecb2dec1371a25cafca56b8394f9818bc
Scampus11 <103895051+Scampus11@users.noreply.github.com>
2024-01-17 23:26:44 +0530

    JobTitle master added

---
+++ ScampusCloud/Controllers/JobTitleController.cs
@@ -0,0 +1,259 @@
+﻿using ClosedXML.Excel;
+using ScampusCloud.Models;
+using ScampusCloud.Repository.JobTitle;
+using ScampusCloud.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScampusCloud.Controllers
+{
+    public class JobTitleController : Controller
+    {
+        #region Variable Declaration
+        private readonly JobTitleRepository _JobTitleRepository;
+        JobTitleModel _JobTitleModel = new JobTitleModel();
+        #endregion
+        #region CTOR
+        public JobTitleController()
+        {
+            _JobTitleRepository = new JobTitleRepository();
+        }
+        #endregion
+
+        #region Method
+        // GET: JobTitle
+        public ActionResult JobTitle()
+        {
+            ViewData["paging_size"] = 10;
+            if (ViewData["currentPage"] == null)
+                ViewData["currentPage"] = 1;
+            string searchtxt = "NA";
+            int totals = Convert.ToInt32(_JobTitleRepository.GetAllCount(searchtxt));
+            ViewData["totalrecords"] = totals;
+            return View();
+        }
+        public ActionResult AddEditJobTitle(string ID = "")
+        {
+            try
+            {
+
+                _JobTitleModel.CreatedBy = new Guid(Session["UserId"].ToString());
+                _JobTitleModel.ModifiedBy = new Guid(Session["UserId"].ToString());
+                _JobTitleModel.CompanyId = new Guid(Session["CompanyId"].ToString());
+
+                if (!string.IsNullOrEmpty(ID) && ID != "0")
+                {
+                    #region Get Entity by id
+                    _JobTitleModel.ActionType = "Edit";
+                    _JobTitleModel.Id = Convert.ToInt32(ID);
+                    _JobTitleModel = _JobTitleRepository.AddEdit_JobTitle(_JobTitleModel);
+                    if (_JobTitleModel != null)
+                    {
+                        _JobTitleModel.IsEdit = true;
+
+                        //if (model.Code == null)
+                        //    model.Code = "";
+                        //HttpContext.Session.SetString("Original_Id", model.Code);
+                    }
+                    else
+                    {
+                        _JobTitleModel = new JobTitleModel();
+                        ViewBag.NoRecordExist = true;
+                        _JobTitleModel.Response_Message = "No record found";
+                        //HttpContext.Session.SetString("Original_Id", "");
+                    }
+                    #endregion
+                }
+                //// If url Apeended with Querystring ID=0 then Redirect into current Action
+                else if (!string.IsNullOrEmpty(ID) && ID == "0")
+                {
+                    return RedirectToAction("AddEditJobTitle");
+                }
+                else
+                {
+                    _JobTitleModel.IsEdit = false;
+                    _JobTitleModel.IsActive = true;
+                    //HttpContext.Session.SetString("Original_Id", "");
+                }
+                return View(_JobTitleModel);
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+
+        }
+        [HttpPost]
+        public ActionResult AddEditJobTitle(JobTitleModel _JobTitleModel, string saveAndExit = "")
+        {
+            if (_JobTitleModel.Id > 0)
+            {
+                _JobTitleModel.ActionType = "Update";
+            }
+            else
+            {
+                _JobTitleModel.ActionType = "Insert";
+            }
+            var officemaster = _JobTitleRepository.AddEdit_JobTitle(_JobTitleModel);
+            if (!string.IsNullOrEmpty(saveAndExit))
+            {
+                return RedirectToAction("JobTitle", "JobTitle");
+            }
+            else if (_JobTitleModel.IsEdit == true)
+            {
+                return RedirectToAction("AddEditJobTitle", new { ID = _JobTitleModel.Id });
+            }
+            else
+            {
+                return RedirectToAction("AddEditJobTitle");
+            }
+            //return RedirectToAction("JobTitle", "JobTitle");
+        }
+
+        public ActionResult JobTitleList(int page = 1, int pagesize = 10, string searchtxt = "")
+        {
+            try
+            {
+                searchtxt = string.IsNullOrEmpty(searchtxt) ? "" : searchtxt;
+                int totals = Convert.ToInt32(_JobTitleRepository.GetAllCount(searchtxt));
+                var lstCountries = _JobTitleRepository.GetJobTitleList(searchtxt, page, pagesize);
+                Session["totalrecords"] = Convert.ToString(totals);
+                Session["paging_size"] = Convert.ToString(pagesize);
+                ViewData["totalrecords"] = totals;
+                ViewData["paging_size"] = pagesize;
+                StringBuilder strHTML = new StringBuilder();
+                if (lstCountries.Count > 0)
+                {
+                    strHTML.Append("<table class='datatable-bordered datatable-head-custom datatable-table' id='kt_datatable'>");
+                    strHTML.Append("<thead class='datatable-head'>");
+                    strHTML.Append("<tr class='datatable-row'>");
+                    strHTML.Append("<th class='datatable-cell'>Code</th>");
+                    strHTML.Append("<th class='datatable-cell'>Name</th>");
+                    strHTML.Append("<th class='datatable-cell'>Status</th>");
+                    strHTML.Append("<th class='datatable-cell'>Action</th>");
+                    strHTML.Append("</tr>");
+                    strHTML.Append("</thead>");
+                    strHTML.Append("<tbody class='datatable-body custom-scroll'>");
+                    foreach (var item in lstCountries)
+                    {
+                        string DeleteConfirmationEvent = "DeleteConfirmation('" + item.Id + "','JobTitle','JobTitle','Delete')";
+                        strHTML.Append("<tr>");
+                        strHTML.Append("<td>" + item.Code + "</td>");
+                        strHTML.Append("<td>" + item.Name + "</td>");
+                        if (item.IsActive)
+                            strHTML.Append("<td><span><span class='label font-weight-bold label-lg label-light-primary label-inline'>Active</span></span></td>");
+                        else
+                            strHTML.Append("<td><span><span class='label font-weight-bold label-lg label-light-danger label-inline'>InActive</span></span></td>");
+
+                        strHTML.Append("<td>");
+                        strHTML.Append("<a class='btn btn-sm btn-icon btn-lg-light btn-text-primary btn-hover-light-primary mr-3' href= '/JobTitle/AddEditJobTitle?ID=" + item.Id + "'><i class='flaticon-edit'></i></a>");
+                        strHTML.Append("<a id = 'del_" + item.Id + "' class='btn btn-sm btn-icon btn-lg-light btn-text-danger btn-hover-light-danger' onclick=" + DeleteConfirmationEvent + "><i class='flaticon-delete'></i></a>");
+                        strHTML.Append("</td>");
+                        strHTML.Append("</tr>");
+                    }
+                    strHTML.Append("</tbody>");
+                    strHTML.Append("</table>");
+                }
+                else
+                {
+                    strHTML.Append("<center>No data available in table</center>");
+                }
+                return Content(strHTML.ToString());
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+
+
+            //return Json(JobTitle, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult JobTitleListCount()
+        {
+            try
+            {
+                string result = string.Empty;
+                int totals = Convert.ToInt32(Session["totalrecords"]);
+                int pagesize = Convert.ToInt32(Session["paging_size"]);
+                ViewData["paging_size"] = Convert.ToInt32(Session["paging_size"]);
+
+                int noofpages = 1;
+                if (totals > 0 && pagesize > 0)
+                    noofpages = (totals / pagesize) + (totals % pagesize != 0 ? 1 : 0);
+                result = "{\"noofpages\":" + noofpages + ",\"NoOfTotalRecords\":" + totals + "}";
+
+                return Content((result));
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string Id)
+        {
+            try
+            {
+                _JobTitleModel.ActionType = "Delete";
+                _JobTitleModel.Id = Convert.ToInt32(Id);
+                var response = _JobTitleRepository.AddEdit_JobTitle(_JobTitleModel);
+
+                return RedirectToAction("JobTitle", "JobTitle");
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+        }
+
+        //[HttpPost]
+        //public string PreviewSelectedImage(IFormFile file)
+        //{
+        //    string base64OfThumbImg = string.Empty, originalImageBase64 = string.Empty;
+        //    if (file != null)
+        //    {
+        //        //originalImageBase64 = ImageCompressor.OriginalBase64String(file).Result;
+        //        base64OfThumbImg = ImageCompressor.GetBase64StringAsync(file, 300, 300).Result;
+        //    }
+        //    return base64OfThumbImg;
+        //}
+
+        [HttpGet]
+        public FileResult Export(string searchtxt = "")
+        {
+            DataTable dt = new DataTable("JobTitle");
+            try
+            {
+                dt = _JobTitleRepository.GetJobTitleData_Export(searchtxt);
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    wb.Worksheets.Add(dt);
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        wb.SaveAs(stream);
+                        return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "JobTitle.xlsx");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+        }
+        #endregion
+    }
+}
---
+++ ScampusCloud/Repository/JobTitle/JobTitleRepository.cs
@@ -0,0 +1,108 @@
+﻿using ScampusCloud.DataBase;
+using ScampusCloud.Models;
+using ScampusCloud.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace ScampusCloud.Repository.JobTitle
+{
+    public class JobTitleRepository
+    {
+		private GeneralMethods objgm = new GeneralMethods();
+		public JobTitleModel AddEdit_JobTitle(JobTitleModel _JobTitleModel)
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = _JobTitleModel.GetType().Name,
+					StoredProcedureName = @"SP_JobTitle_CURD",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+
+				objQueryBuilder.AddFieldValue("@Id", _JobTitleModel.Id, DataTypes.Numeric, false);
+				objQueryBuilder.AddFieldValue("@Name", _JobTitleModel.Name, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@CompanyId", _JobTitleModel.CompanyId, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Code", _JobTitleModel.Code, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Isactive", _JobTitleModel.IsActive, DataTypes.Boolean, false);
+				objQueryBuilder.AddFieldValue("@CreatedBy", _JobTitleModel.CreatedBy, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@ModifiedBy", _JobTitleModel.ModifiedBy, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@ActionType", _JobTitleModel.ActionType, DataTypes.Text, false);
+
+				return objgm.ExecuteObjectUsingSp<JobTitleModel>(objQueryBuilder);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+
+		public List<JobTitleModel> GetJobTitleList(string searchtxt = "", int page = 1, int pagesize = 10)
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = "Tbl_Mstr_JobTitle",
+					StoredProcedureName = @"SP_GetJobTitleData",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+				objQueryBuilder.AddFieldValue("@Search", searchtxt, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@page", page, DataTypes.Numeric, false);
+				objQueryBuilder.AddFieldValue("@pagesize", pagesize, DataTypes.Numeric, false);
+				return objgm.GetListUsingSp<JobTitleModel>(objQueryBuilder);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+
+		public string GetAllCount(string searchtxt = "")
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = "Tbl_Mstr_JobTitle",
+					StoredProcedureName = @"SP_GetJobTitleData_Count",
+					SetQueryType = QueryBuilder.QueryType.SELECT,
+				};
+				objQueryBuilder.AddFieldValue("@Search", searchtxt, DataTypes.Text, false);
+				return objgm.ExcecuteScalarUsingSp(objQueryBuilder);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+
+		}
+
+		public DataTable GetJobTitleData_Export(string searchtxt = "")
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = "JobTitle",
+					StoredProcedureName = @"SP_DownloadJobTitleData_Export",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+				objQueryBuilder.AddFieldValue("@Search", searchtxt, DataTypes.Text, false);
+				return objgm.ExecuteDataTableUsingSp(objQueryBuilder, true);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+	}
+}
