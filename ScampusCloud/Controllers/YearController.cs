4d5978b7833ccdfaa9e30aeacfba4d7c86c689db
Scampus11 <103895051+Scampus11@users.noreply.github.com>
2024-01-20 03:11:16 +0530

    Year master added

---
+++ ScampusCloud/Controllers/YearController.cs
@@ -0,0 +1,259 @@
+﻿using ClosedXML.Excel;
+using ScampusCloud.Models;
+using ScampusCloud.Repository.Year;
+using ScampusCloud.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScampusCloud.Controllers
+{
+    public class YearController : Controller
+    {
+        #region Variable Declaration
+        private readonly YearRepository _YearRepository;
+        YearModel _YearModel = new YearModel();
+        #endregion
+        #region CTOR
+        public YearController()
+        {
+            _YearRepository = new YearRepository();
+        }
+        #endregion
+
+        #region Method
+        // GET: Year
+        public ActionResult Year()
+        {
+            ViewData["paging_size"] = 10;
+            if (ViewData["currentPage"] == null)
+                ViewData["currentPage"] = 1;
+            string searchtxt = "NA";
+            int totals = Convert.ToInt32(_YearRepository.GetAllCount(searchtxt));
+            ViewData["totalrecords"] = totals;
+            return View();
+        }
+        public ActionResult AddEditYear(string ID = "")
+        {
+            try
+            {
+
+                _YearModel.CreatedBy = new Guid(Session["UserId"].ToString());
+                _YearModel.ModifiedBy = new Guid(Session["UserId"].ToString());
+                _YearModel.CompanyId = new Guid(Session["CompanyId"].ToString());
+
+                if (!string.IsNullOrEmpty(ID) && ID != "0")
+                {
+                    #region Get Entity by id
+                    _YearModel.ActionType = "Edit";
+                    _YearModel.Id = Convert.ToInt32(ID);
+                    _YearModel = _YearRepository.AddEdit_Year(_YearModel);
+                    if (_YearModel != null)
+                    {
+                        _YearModel.IsEdit = true;
+
+                        //if (model.Code == null)
+                        //    model.Code = "";
+                        //HttpContext.Session.SetString("Original_Id", model.Code);
+                    }
+                    else
+                    {
+                        _YearModel = new YearModel();
+                        ViewBag.NoRecordExist = true;
+                        _YearModel.Response_Message = "No record found";
+                        //HttpContext.Session.SetString("Original_Id", "");
+                    }
+                    #endregion
+                }
+                //// If url Apeended with Querystring ID=0 then Redirect into current Action
+                else if (!string.IsNullOrEmpty(ID) && ID == "0")
+                {
+                    return RedirectToAction("AddEditYear");
+                }
+                else
+                {
+                    _YearModel.IsEdit = false;
+                    _YearModel.IsActive = true;
+                    //HttpContext.Session.SetString("Original_Id", "");
+                }
+                return View(_YearModel);
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+
+        }
+        [HttpPost]
+        public ActionResult AddEditYear(YearModel _YearModel, string saveAndExit = "")
+        {
+            if (_YearModel.Id > 0)
+            {
+                _YearModel.ActionType = "Update";
+            }
+            else
+            {
+                _YearModel.ActionType = "Insert";
+            }
+            var officemaster = _YearRepository.AddEdit_Year(_YearModel);
+            if (!string.IsNullOrEmpty(saveAndExit))
+            {
+                return RedirectToAction("Year", "Year");
+            }
+            else if (_YearModel.IsEdit == true)
+            {
+                return RedirectToAction("AddEditYear", new { ID = _YearModel.Id });
+            }
+            else
+            {
+                return RedirectToAction("AddEditYear");
+            }
+            //return RedirectToAction("Year", "Year");
+        }
+
+        public ActionResult YearList(int page = 1, int pagesize = 10, string searchtxt = "")
+        {
+            try
+            {
+                searchtxt = string.IsNullOrEmpty(searchtxt) ? "" : searchtxt;
+                int totals = Convert.ToInt32(_YearRepository.GetAllCount(searchtxt));
+                var lstCountries = _YearRepository.GetYearList(searchtxt, page, pagesize);
+                Session["totalrecords"] = Convert.ToString(totals);
+                Session["paging_size"] = Convert.ToString(pagesize);
+                ViewData["totalrecords"] = totals;
+                ViewData["paging_size"] = pagesize;
+                StringBuilder strHTML = new StringBuilder();
+                if (lstCountries.Count > 0)
+                {
+                    strHTML.Append("<table class='datatable-bordered datatable-head-custom datatable-table' id='kt_datatable'>");
+                    strHTML.Append("<thead class='datatable-head'>");
+                    strHTML.Append("<tr class='datatable-row'>");
+                    strHTML.Append("<th class='datatable-cell'>Code</th>");
+                    strHTML.Append("<th class='datatable-cell'>Year</th>");
+                    strHTML.Append("<th class='datatable-cell'>Status</th>");
+                    strHTML.Append("<th class='datatable-cell'>Action</th>");
+                    strHTML.Append("</tr>");
+                    strHTML.Append("</thead>");
+                    strHTML.Append("<tbody class='datatable-body custom-scroll'>");
+                    foreach (var item in lstCountries)
+                    {
+                        string DeleteConfirmationEvent = "DeleteConfirmation('" + item.Id + "','Year','Year','Delete')";
+                        strHTML.Append("<tr>");
+                        strHTML.Append("<td>" + item.Code + "</td>");
+                        strHTML.Append("<td>" + item.Name + "</td>");
+                        if (item.IsActive)
+                            strHTML.Append("<td><span><span class='label font-weight-bold label-lg label-light-primary label-inline'>Active</span></span></td>");
+                        else
+                            strHTML.Append("<td><span><span class='label font-weight-bold label-lg label-light-danger label-inline'>InActive</span></span></td>");
+
+                        strHTML.Append("<td>");
+                        strHTML.Append("<a class='btn btn-sm btn-icon btn-lg-light btn-text-primary btn-hover-light-primary mr-3' href= '/Year/AddEditYear?ID=" + item.Id + "'><i class='flaticon-edit'></i></a>");
+                        strHTML.Append("<a id = 'del_" + item.Id + "' class='btn btn-sm btn-icon btn-lg-light btn-text-danger btn-hover-light-danger' onclick=" + DeleteConfirmationEvent + "><i class='flaticon-delete'></i></a>");
+                        strHTML.Append("</td>");
+                        strHTML.Append("</tr>");
+                    }
+                    strHTML.Append("</tbody>");
+                    strHTML.Append("</table>");
+                }
+                else
+                {
+                    strHTML.Append("<center>No data available in table</center>");
+                }
+                return Content(strHTML.ToString());
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+
+
+            //return Json(Year, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult YearListCount()
+        {
+            try
+            {
+                string result = string.Empty;
+                int totals = Convert.ToInt32(Session["totalrecords"]);
+                int pagesize = Convert.ToInt32(Session["paging_size"]);
+                ViewData["paging_size"] = Convert.ToInt32(Session["paging_size"]);
+
+                int noofpages = 1;
+                if (totals > 0 && pagesize > 0)
+                    noofpages = (totals / pagesize) + (totals % pagesize != 0 ? 1 : 0);
+                result = "{\"noofpages\":" + noofpages + ",\"NoOfTotalRecords\":" + totals + "}";
+
+                return Content((result));
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string Id)
+        {
+            try
+            {
+                _YearModel.ActionType = "Delete";
+                _YearModel.Id = Convert.ToInt32(Id);
+                var response = _YearRepository.AddEdit_Year(_YearModel);
+
+                return RedirectToAction("Year", "Year");
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+        }
+
+        //[HttpPost]
+        //public string PreviewSelectedImage(IFormFile file)
+        //{
+        //    string base64OfThumbImg = string.Empty, originalImageBase64 = string.Empty;
+        //    if (file != null)
+        //    {
+        //        //originalImageBase64 = ImageCompressor.OriginalBase64String(file).Result;
+        //        base64OfThumbImg = ImageCompressor.GetBase64StringAsync(file, 300, 300).Result;
+        //    }
+        //    return base64OfThumbImg;
+        //}
+
+        [HttpGet]
+        public FileResult Export(string searchtxt = "")
+        {
+            DataTable dt = new DataTable("Year");
+            try
+            {
+                dt = _YearRepository.GetYearData_Export(searchtxt);
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    wb.Worksheets.Add(dt);
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        wb.SaveAs(stream);
+                        return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Year.xlsx");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+                throw;
+            }
+        }
+        #endregion
+    }
+}
