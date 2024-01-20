616a041c20738b0bdbd83102c5693d4a5c35c35e
Scampus11 <103895051+Scampus11@users.noreply.github.com>
2024-01-20 12:01:17 +0530

    Admission master added

---
+++ ScampusCloud/Models/AdmissionModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScampusCloud.Models
+{
+    public class AdmissionModel : ResponseMessage
+    {
+        public int? Id { get; set; }
+
+        public Guid? CompanyId { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+        public bool IsEdit { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime dtCreatedDate { get; set; }
+        public DateTime dtModifiedDate { get; set; }
+
+        [Required(ErrorMessage = "Enter Code id")]
+        //[Remote(action: "IsAdmissionIdExist", controller: "RemoteValidation")]
+        public string Code { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public string ActionType { get; set; }
+    }
+}
---
+++ ScampusCloud/Repository/Admission/AdmissionRepository.cs
@@ -0,0 +1,109 @@
+﻿using ScampusCloud.DataBase;
+using ScampusCloud.Models;
+using ScampusCloud.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace ScampusCloud.Repository.Admission
+{
+	public class AdmissionRepository
+	{
+		private GeneralMethods objgm = new GeneralMethods();
+		public AdmissionModel AddEdit_Admission(AdmissionModel _AdmissionModel)
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = _AdmissionModel.GetType().Name,
+					StoredProcedureName = @"SP_Admission_CURD",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+
+				objQueryBuilder.AddFieldValue("@Id", _AdmissionModel.Id, DataTypes.Numeric, false);
+				objQueryBuilder.AddFieldValue("@Name", _AdmissionModel.Name, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Type", _AdmissionModel.Type, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@CompanyId", _AdmissionModel.CompanyId, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Code", _AdmissionModel.Code, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@Isactive", _AdmissionModel.IsActive, DataTypes.Boolean, false);
+				objQueryBuilder.AddFieldValue("@CreatedBy", _AdmissionModel.CreatedBy, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@ModifiedBy", _AdmissionModel.ModifiedBy, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@ActionType", _AdmissionModel.ActionType, DataTypes.Text, false);
+
+				return objgm.ExecuteObjectUsingSp<AdmissionModel>(objQueryBuilder);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+
+		public List<AdmissionModel> GetAdmissionList(string searchtxt = "", int page = 1, int pagesize = 10)
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = "Tbl_Mstr_Admission",
+					StoredProcedureName = @"SP_GetAdmissionData",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+				objQueryBuilder.AddFieldValue("@Search", searchtxt, DataTypes.Text, false);
+				objQueryBuilder.AddFieldValue("@page", page, DataTypes.Numeric, false);
+				objQueryBuilder.AddFieldValue("@pagesize", pagesize, DataTypes.Numeric, false);
+				return objgm.GetListUsingSp<AdmissionModel>(objQueryBuilder);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+
+		public string GetAllCount(string searchtxt = "")
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = "Tbl_Mstr_Admission",
+					StoredProcedureName = @"SP_GetAdmissionData_Count",
+					SetQueryType = QueryBuilder.QueryType.SELECT,
+				};
+				objQueryBuilder.AddFieldValue("@Search", searchtxt, DataTypes.Text, false);
+				return objgm.ExcecuteScalarUsingSp(objQueryBuilder);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+
+		}
+
+		public DataTable GetAdmissionData_Export(string searchtxt = "")
+		{
+			try
+			{
+				QueryBuilder objQueryBuilder = new QueryBuilder
+				{
+					TableName = "Admission",
+					StoredProcedureName = @"SP_DownloadAdmissionData_Export",
+					SetQueryType = QueryBuilder.QueryType.SELECT
+				};
+				objQueryBuilder.AddFieldValue("@Search", searchtxt, DataTypes.Text, false);
+				return objgm.ExecuteDataTableUsingSp(objQueryBuilder, true);
+			}
+			catch (Exception ex)
+			{
+				ErrorLogger.WriteToErrorLog(ex.Message, ex.StackTrace, ex.InnerException != null ? ex.InnerException.ToString() : string.Empty, this.GetType().Name + " : " + MethodBase.GetCurrentMethod().Name);
+				throw;
+			}
+		}
+	}
+}
