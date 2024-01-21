4a16f32c84324b5f2fe506d16c4223dce9410154
Scampus11 <103895051+Scampus11@users.noreply.github.com>
2024-01-21 22:14:50 +0530

    guid to string changes

--- ScampusCloud/Models/UserModel.cs
+++ ScampusCloud/Models/UserModel.cs
@@ -10,10 +10,7 @@ namespace ScampusCloud.Models
     {
         public int Id { get; set; }
         public Guid UserId { get; set; }
-
-        public Guid StaffId { get; set; }
-
-       
+        public string StaffId { get; set; }
         public string Name { get; set; }
         public string EmailId { get; set; }
         public string PhoneNumber { get; set; }
